c6c09b8b13c64aa2f7223a824f0e311e7cc2e71b
Alaina <122501846+alaina-holland@users.noreply.github.com>
2024-06-13 14:31:08 -0700

    Update mapper.py
    Updated to :
    Use regular expressions to match contiguous strings of letters.
    Convert words to lowercase.
    Ignore non-letter characters.

--- mapper.py
+++ mapper.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 # -*-coding:utf-8 -*
 import sys 
-  
+import re
+
+# Regular expression to match contiguous strings of letters
+word_pattern = re.compile(r'[a-zA-Z]+')
+
 for line in sys.stdin: 
     line = line.strip() 
-    words = line.split() 
+    words = word_pattern.findall(line)
     for word in words:
-        print('%s\t%s' % (word, 1))
+        print(f'{word.lower()}\t1')
